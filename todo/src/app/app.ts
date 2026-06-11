1ba27ca3e3ec97e60d59c7e798c92e259f99fc48
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-06-11 16:53:39 +0530

    update todo

--- todo/src/app/app.ts
+++ todo/src/app/app.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('todo');
+      title = 'Todo Application';
+      message = 'welcome to in28minutes';
 }
