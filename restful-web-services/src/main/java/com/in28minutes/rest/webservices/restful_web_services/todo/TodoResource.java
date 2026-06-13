0198a355630e9de7b00e1abaaafa743614471778
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-06-13 22:39:54 +0530

    created rest api for delete

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/todo/TodoResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/todo/TodoResource.java
@@ -3,7 +3,8 @@ package com.in28minutes.rest.webservices.restful_web_services.todo;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,19 @@ public class TodoResource {
     @GetMapping("/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username) {
         return todoService.findAll();
+    }
+
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo getTodo(@PathVariable String username, @PathVariable long id) {
+        return todoService.findById(id);
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Object> deleteTodo(@PathVariable String username, @PathVariable long id) {
+        Todo todo = todoService.deleteById(id);
+        if(todo != null) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
--- todo-frontend/src/app/list-todos-component/list-todos-component.ts
+++ todo-frontend/src/app/list-todos-component/list-todos-component.ts
@@ -19,15 +19,36 @@ export class Todo {
 export class ListTodosComponent implements OnInit {
   
   todos:Todo[] = [];
+  message: any;
   
   constructor(private todoData: TodoData) {}
   
   ngOnInit(){
+    this.refreshTodos();
+  }
+
+  refreshTodos(){
     this.todoData.retrieveAllTodos('in28minutes').subscribe(
       response => { console.log(response);
     this.todos = response;})
   }
-  
+
+  deleteTodo(id:any) 
+  {
+     console.log(`Delete todo ${id}`);
+     this.todoData.deleteTodo('in28minutes', id).subscribe(
+      response => {
+        console.log(response);
+        this.message = `Delete of todo ${id} successful!`;
+        this.refreshTodos();
+      })
+  }
+
+  updateTodo(id:any) 
+  {
+    console.log(`Update todo ${id}`);
+  }
+
   // todos: Todo[] = [
   //   new Todo(1, 'Learn to Dance', false, new Date()),
   //   new Todo(2, 'Become an Angular Expert', false, new Date()),
